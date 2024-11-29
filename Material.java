2be405b782973a58759fb9eb52758cf6fb4a5837
tamimiyah <160693673+tamimiyah@users.noreply.github.com>
2024-11-30 03:55:14 +0800

    Update Material.java

--- Material.java
+++ Material.java
@@ -1,24 +1,85 @@
-public abstract class Material {
-    private String materialID;
+public class Material {
+
+    private String materialID, yearPublished, publisher;
+
     private int copies;
 
-    public Material(String materialID, int copies) {
+
+
+    public Material(String materialID, String yearPublished, String publisher, int copies) {
+
         this.materialID = materialID;
+
+        this.yearPublished = yearPublished;
+
+        this.publisher = publisher;
+
         this.copies = copies;
+
     }
+
+
 
     public String getMaterialID() {
+
         return materialID;
+
     }
+
+
+
+    public void setMaterialID(String materialID) {
+
+        this.materialID = materialID;
+
+    }
+
+
+
+    public String getYearPublished() {
+
+        return yearPublished;
+
+    }
+
+
+
+    public void setYearPublished(String yearPublished) {
+
+        this.yearPublished = yearPublished;
+
+    }
+
+
+
+    public String getPublisher() {
+
+        return publisher;
+
+    }
+
+
+
+    public void setPublisher(String publisher) {
+
+        this.publisher = publisher;
+
+    }
+
+
 
     public int getCopies() {
+
         return copies;
+
     }
+
+
 
     public void setCopies(int copies) {
+
         this.copies = copies;
+
     }
 
-    @Override
-    public abstract String toString();
 }
